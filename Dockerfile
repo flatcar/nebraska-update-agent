73fefd6ec8778dd643c14dec33bfa14291d47a96
Suraj Deshmukh <suraj@kinvolk.io>
2021-10-05 14:35:01 +0530

    docker-build: Convert the commands to run on any arch
    Signed-off-by: Suraj Deshmukh <suraj@kinvolk.io>

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,4 @@
 FROM registry.fedoraproject.org/fedora:34
+ARG ARCH=amd64
 COPY luc /bin/
 ENTRYPOINT [ "/bin/luc" ]
--- hack/build.sh
+++ hack/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-go build -o luc main.go
-docker build -t quay.io/kinvolk/luc .
+env GOOS=linux GOARCH=amd64 go build -o luc main.go
+docker buildx build --load -t quay.io/kinvolk/luc --platform linux/amd64 .
